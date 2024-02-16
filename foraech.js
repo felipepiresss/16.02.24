c4b94a76c78dc5ee0acd0e3ce61ff929e8583862
Felipe <142436351+felipepiresss@users.noreply.github.com>
2024-02-16 12:27:12 -0300

    Add files via upload

---
+++ foraech.js
@@ -0,0 +1,12 @@
+var tempoFilme = 60; // em segundos
+var inicioFilme = 0; // em segundos
+
+for (var t = 0; t < tempoFilme; t++) {
+    setTimeout(function() {
+        console.clear();
+        console.log("Assistindo fileme 60 segundos... Ainda faltam [" + (tempoFilme -
+        inicioFilme) + "] segundos.");
+        inicioFilme++;
+        },t*1000);
+
+}
